b65b853cffcca54755deee2feec150ce81fcc10e
says33 <says.rodriguez@gmail.com>
2016-06-27 11:40:51 -0500

    #master se cambia el return null por una instancia del fragmento de register

--- app/src/main/groovy/com/smart/cities/ui/activity/RegisterActivity.groovy
+++ app/src/main/groovy/com/smart/cities/ui/activity/RegisterActivity.groovy
@@ -1,11 +1,20 @@
 package com.smart.cities.ui.activity
 
+import android.content.Context
+import android.content.Intent
 import android.support.v4.app.Fragment
 import com.smart.cities.commons.SingleFragmentActivity
+import com.smart.cities.ui.fragment.RegisterFragment
 
 class RegisterActivity extends SingleFragmentActivity {
+
+    static Intent newIntentWithContext(Context context){
+        Intent intent = new Intent(context, RegisterActivity)
+        intent
+    }
+
     @Override
     Fragment createFragment() {
-        return null
+        new RegisterFragment()
     }
 }
